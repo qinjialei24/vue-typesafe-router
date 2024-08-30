648c6aad3f2a1337d64e44b0ec61881bb1908446
qinjialei24 <954551222@qq.com>
2024-08-30 17:09:24 +0800

    refactor: replace rollup with vite

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueTypesafeRouter',
+      fileName: (format) => `vue-typesafe-router.${format}.js`,
+      formats: ['es', 'cjs','umd'],
+    },
+    rollupOptions: {
+      external: ['vue', 'vue-router'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'vue-router': 'VueRouter',
+        },
+      },
+    },
+  },
+})
