d8a68848324b17f359f58200cfbac69b27a79eba
qinjialei24 <954551222@qq.com>
2024-09-12 16:26:43 +0800

    chore: improve config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
@@ -12,19 +12,19 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VueTypesafeRouter',
-      fileName: (format) => `vue-typesafe-router.${format}.js`,
-      formats: ['es', 'cjs','umd'],
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "VueTypesafeRouter",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ['vue', 'vue-router'],
+      external: ["vue", "vue-router"],
       output: {
         globals: {
-          vue: 'Vue',
-          'vue-router': 'VueRouter',
+          vue: "Vue",
+          "vue-router": "VueRouter",
         },
       },
     },
   },
-})
+});
